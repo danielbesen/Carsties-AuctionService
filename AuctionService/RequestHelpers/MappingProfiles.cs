b10f349b7fb22289f9dad4eb55dd4da64d67afa7
Daniel Besen <nielbesen@gmail.com>
2024-06-09 20:24:08 -0300

    Added update auction method; Added create auction method; Added mapping configs to ignore null and zero values from the source class.

--- AuctionService/RequestHelpers/MappingProfiles.cs
+++ AuctionService/RequestHelpers/MappingProfiles.cs
@@ -7,10 +7,33 @@ namespace AuctionService.RequestHelpers
     public class MappingProfiles : Profile
     {
         public MappingProfiles() {
-            CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+            CreateMap<Auction, AuctionDto>()
+                .IncludeMembers(x => x.Item);
+
             CreateMap<Item, AuctionDto>();
-            CreateMap<CreateAuctionDto, Auction>().ForMember(d => d.Item, o => o.MapFrom(s => s));
+
+            CreateMap<CreateAuctionDto, Auction>()
+                .ForMember(d => d.Item, o => o.MapFrom(s => s));
+
             CreateMap<CreateAuctionDto, Item>();
+
+            CreateMap<UpdateAuctionDto, Auction>()
+                .ForMember(d => d.Item, o => o.MapFrom(s => s))
+                .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<UpdateAuctionDto, Item>()
+                .ForAllMembers(opt => opt.Condition((src, dest, srcMember) =>
+                {
+                    if (srcMember == null)
+                        return false;
+
+                    if (srcMember is int intValue)
+                    {
+                        return intValue != 0;
+                    }
+
+                    return true;
+                }));
         }
     }
 }
