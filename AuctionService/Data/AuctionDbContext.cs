01ceb283ea846c11cc82ab6681e2c3d05c255caa
Daniel Besen <nielbesen@gmail.com>
2024-06-23 22:37:06 -0300

    Added message outbokx

--- AuctionService/Data/AuctionDbContext.cs
+++ AuctionService/Data/AuctionDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using AuctionService.Entities;
 using Microsoft.EntityFrameworkCore;
+using MassTransit;
 
 namespace AuctionService.Data
 {
@@ -10,5 +11,14 @@ namespace AuctionService.Data
         }
 
         public DbSet<Auction> Auctions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.AddInboxStateEntity();
+            modelBuilder.AddOutboxMessageEntity();
+            modelBuilder.AddOutboxStateEntity();
+        }
     }
 }
