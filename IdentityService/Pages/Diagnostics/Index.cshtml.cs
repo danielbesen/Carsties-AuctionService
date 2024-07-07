8d37b21c992c6ab8ece12c346c3c955c863f0684
Daniel Besen <nielbesen@gmail.com>
2024-07-07 18:41:20 -0300

    RemoteAddress keep changing, so I removed the verification

--- IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -16,7 +16,7 @@ namespace IdentityService.Pages.Diagnostics
 
         public async Task<IActionResult> OnGet()
         {
-            var localAddresses = new List<string?> { "127.0.0.1", "::1" };
+            var localAddresses = new List<string?> { "::ffff:172.18.0.1", "127.0.0.1", "::1" };
 
             Console.WriteLine($"REMOTE IP: {HttpContext.Connection.RemoteIpAddress}");
 
@@ -25,10 +25,10 @@ namespace IdentityService.Pages.Diagnostics
                 localAddresses.Add(HttpContext.Connection.LocalIpAddress.ToString());
             }
 
-            if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress?.ToString()))
-            {
-                return NotFound();
-            }
+            //if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress?.ToString()))
+            //{
+            //    return NotFound();
+            //}
 
             View = new ViewModel(await HttpContext.AuthenticateAsync());
 
