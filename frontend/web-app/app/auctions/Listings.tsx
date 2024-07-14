da2f5ec5f9638df9c6193afb37cb25e4a7222ef8
Daniel Besen <nielbesen@gmail.com>
2024-07-14 18:05:40 -0300

    Added AuctionCard component

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,9 +1,12 @@
+import AuctionCard from "./AuctionCard";
 
 async function getData(){
     const res = await fetch("http://localhost:6001/search");
 
     if (!res.ok)
-        throw new Error('Failed to fetch data')
+        throw new Error('Failed to fetch data');
+
+    return res.json();
 }
 
 export default async function Listings() {
@@ -11,7 +14,9 @@ export default async function Listings() {
 
     return (
         <div>
-            {JSON.stringify(data, null, 2)}
+            {data && data.results.map((auction: any) => (
+                <AuctionCard auction={auction} key= {auction.id}></AuctionCard>
+            ))}
         </div>
     )
 }
